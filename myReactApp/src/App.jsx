d1e25eb18f9e9a1a1c1acb4324d02198c95f3b4c
Misra-Praveen <vgipra@gmail.com>
2025-12-28 01:14:32 +0530

    Landing Screen page Done

---
+++ myReactApp/src/App.jsx
@@ -0,0 +1,13 @@
+
+import LandingScreen from './components/LandingScreen'
+
+function App() {
+
+  return (
+    <>
+      <LandingScreen />
+    </>
+  )
+}
+
+export default App
---
+++ myReactApp/src/components/LandingScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingScreen = () => {
+  return (
+    <div id='landingScreen'>
+        <h1>Welcome to PopX</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        <button className='btn' id='btn-create'>Create Account</button>
+        <button className='btn' id='btn-login'>Already Registered? Login</button>
+    </div>
+  )
+}
+
+export default LandingScreen
